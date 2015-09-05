28cc5bf6dae8d196efcf712a713b06bba4334b45
Raly123 <779628578@qq.com>
2015-09-05 14:58:11 +0800

    增加asmack源代码

---
+++ src/com/tcl/wechat/utils/CharsetDetector.java
@@ -0,0 +1,31 @@
+package com.tcl.wechat.utils;
+
+import info.monitorenter.cpdetector.io.JChardetFacade;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * 检测当前文件的编码方式
+ * @author rex.lei
+ *
+ */
+public class CharsetDetector {
+	
+	public static Charset detect(InputStream in) {
+		JChardetFacade detector = JChardetFacade.getInstance();
+		Charset charset = null;
+		try {
+			in.mark(100);
+			charset = detector.detectCodepage(in, 100);
+			in.reset();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return charset;
+	}
+	
+}
